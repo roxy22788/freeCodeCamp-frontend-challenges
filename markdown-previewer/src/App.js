ebf89922a883ed4e2a305a2af13d495aa3fd2c8c
Fábio Oliveira <roxy2298gm@gmail.com>
2022-01-24 18:30:13 -0300

    teste marked

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,7 +1,17 @@
+import React, { useState } from 'react';
+import { marked } from 'marked'
+import { useEffect } from 'react/cjs/react.development';
+
 function App() {
+  const [html, setHtml] = useState('');
+
+  useEffect(() => {
+    let seila = marked.parse('# Marked in Node.js\n\nRendered by **marked**.');
+    setHtml(seila);
+  });
+
   return (
-    <div>
-    </div>
+    <div dangerouslySetInnerHTML={{__html: html}} />
   );
 }
 
